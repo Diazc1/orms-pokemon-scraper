50e126184e6a3b8b3ed303e932cd0e5ccca79c68
Cathy Diaz <Cathydiaz14@gmail.com>
2021-03-05 16:41:07 -0800

    commit tree 1829ffe238e3600ff9497176c501702cae6498d3

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,33 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:nil, name:, type:, db:)
+        @id = id 
+        @name = name 
+        @type = type
+        @db = db
+    end
+
+
+#Saves an instance of a pokemon with the correct id
+    def self.save(name, type, db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+
+        db.execute(sql, name, type)
+    end
+
+
+#finds a pokemon from the database by their id number and returns a new Pokemon object
+    def self.find(id, db)
+        sql = "SELECT * FROM pokemon WHERE id = ?"
+
+        result = db.execute(sql, id)[0]
+        Pokemon.new(id:result[0], name:result[1], type:result[2], db:db)
+    end
+
+
 end
